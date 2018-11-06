020970d0b896ffe394c614109cc5b991be7c8802
Shu52 <shusajid@gmail.com>
2018-11-06 11:48:51 -0600

    MVP, FUCK YEAH!

---
+++ SotDLCharGen/SotDLCharGen/ViewModels/HumanEditViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using SotDLCharGen.Data;
+using SotDLCharGen.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SotDLCharGen.ViewModels
+{
+    public class HumanEditViewModel
+    {
+
+        public Character Character { get; set; }
+        //[Key]
+        //public int CharacterId { get; set; }
+
+        //[StringLength(30, ErrorMessage = "Max Character limit is 30 characters"), Required]
+        //public string CharacterName { get; set; }
+
+        //[StringLength(10, ErrorMessage = "Max Character limit is 10 characters"), Required]
+        //public string Gender { get; set; }
+
+        //[Range(0, 10,
+        //ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        //public int Level { get; set; }
+
+        //[Display(Name = "Player Name")]
+        //public string ApplicationUserId { get; set; }
+
+        //public ApplicationUser ApplicationUser { get; set; }
+
+        //[Display(Name = "Ancestry")]
+        //public int AncestryId { get; set; }
+
+        //public Ancestry Ancestry { get; set; }
+
+        //public virtual ICollection<CharTrait> CharTraits { get; set; }
+ 
+
+        //public HumanEditViewModel(ApplicationDbContext context)
+        //{
+
+        //}
+        //public HumanEditViewModel() { }
+    }
+}
