82ed742a3c64640dcfe974ad9658323ee0a082cd
Shu52 <shusajid@gmail.com>
2018-11-30 14:17:13 -0600

    Creation of clockworkpurposeviewmodel

---
+++ SotDLCharGen/SotDLCharGen/ViewModels/Clockwork/ClockworkPurposeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using SotDLCharGen.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SotDLCharGen.ViewModels.Clockwork
+{
+    public class ClockworkPurposeViewModel
+    {
+        [Display(Name = "Clockwork Purpose", GroupName = "Clockwork Purpose")]
+        public List<SelectListItem> ClockworkPurpose { get; set; }
+
+        public ClockworkPurposeViewModel(ApplicationDbContext context)
+        {
+
+            ClockworkPurpose = context.ClockworkPurposes.Select(purpose =>
+            new SelectListItem { Text = purpose.ClockworkPurposeValue, Value = purpose.ClockworkPurposeId.ToString() }).ToList();
+
+        }
+    }
+}
