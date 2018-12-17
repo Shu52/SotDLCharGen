5746a38bb638c2075d244e7b146aa2be37dab1ea
Shu52 <shusajid@gmail.com>
2018-12-17 14:47:36 -0600

    Hide non selectable buttons, disable would still allow focus

--- SotDLCharGen/SotDLCharGen/wwwroot/js/clockworkButtons.js
+++ SotDLCharGen/SotDLCharGen/wwwroot/js/clockworkButtons.js
@@ -5,16 +5,17 @@ const strBtn = document.getElementById("strength");
 const aglBtn = document.getElementById("agility");
 const intBtn = document.getElementById("intellect");
 const willBtn = document.getElementById("will");
+let attributeSelected = "";
 //assign dom element to variable for testing
 let changedText = document.getElementById('changed');
 //prime changed on load
 changedText.textContent = "1";
 //options for primed value on load
 if (changedText.textContent === "1") {
-    strBtn.classList.remove("disabled");
-    aglBtn.classList.remove("disabled");
-    intBtn.classList.add("disabled");
-    willBtn.classList.add("disabled");
+    strBtn.classList.remove("hidden");
+    aglBtn.classList.remove("hidden");
+    intBtn.classList.add("hidden");
+    willBtn.classList.add("hidden");
     console.log(changedText, "option 1")
 }
 
@@ -22,36 +23,47 @@ if (changedText.textContent === "1") {
 function listQ() {
     changedText.textContent = this.value;
     if (changedText.textContent === "1") {
-        strBtn.classList.remove("disabled");
-        aglBtn.classList.remove("disabled");
-        intBtn.classList.add("disabled");
-        willBtn.classList.add("disabled");
+        strBtn.classList.remove("hidden");
+        aglBtn.classList.remove("hidden");
+        intBtn.classList.add("hidden");
+        willBtn.classList.add("hidden");
     }
     if (changedText.textContent === "2") {
-        strBtn.classList.remove("disabled");
-        aglBtn.classList.add("disabled");
-        intBtn.classList.add("disabled");
-        willBtn.classList.add("disabled");
+        strBtn.classList.remove("hidden");
+        aglBtn.classList.add("hidden");
+        intBtn.classList.add("hidden");
+        willBtn.classList.add("hidden");
     }
     if (changedText.textContent === "3") {
-        strBtn.classList.add("disabled");
-        aglBtn.classList.add("disabled");
-        intBtn.classList.remove("disabled");
-        willBtn.classList.remove("disabled");
+        strBtn.classList.add("hidden");
+        aglBtn.classList.add("hidden");
+        intBtn.classList.remove("hidden");
+        willBtn.classList.remove("hidden");
     }
     if (changedText.textContent === "4") {
-        strBtn.classList.add("disabled");
-        aglBtn.classList.remove("disabled");
-        intBtn.classList.remove("disabled");
-        willBtn.classList.add("disabled");
+        strBtn.classList.add("hidden");
+        aglBtn.classList.remove("hidden");
+        intBtn.classList.remove("hidden");
+        willBtn.classList.add("hidden");
     }
 
     if (changedText.textContent === "5") {
-        strBtn.classList.remove("disabled");
-        aglBtn.classList.remove("disabled");
-        intBtn.classList.remove("disabled");
-        willBtn.classList.remove("disabled");
+        strBtn.classList.remove("hidden");
+        aglBtn.classList.remove("hidden");
+        intBtn.classList.remove("hidden");
+        willBtn.classList.remove("hidden");
     }
 }
 //get value on change to call function
 document.getElementById("ClockworkPurposeId").onchange = listQ;
+
+function focusedAttribute(attributeElement) {
+    if (attributeElement.classList.contains("hidden")) {
+        console.log("Not allowed")
+    }
+    else {
+        attributeSelected = attributeElement.id;
+        console.log(attributeSelected, attributeElement.id);
+    }
+}
+console.log("class name", document.getElementById('will').classList.contains("hidden"))
